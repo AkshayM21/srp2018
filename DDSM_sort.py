834411a16d9c489e527cc5ef7c5549e7d2e2db33
AkshayM21 <adventurechooseyour@gmail.com>
2017-12-06 19:15:40 -0800

    ddsm sort
    added to if storage runs low

--- DDSM_sort.py
+++ DDSM_sort.py
@@ -1,3 +1,14 @@
+import os
+import shutil
+
+#changes ddsm variable to not have anything with cc in it
+#quick fix
 def weed_out(DDSM):
     DDSM = [x for x in DDSM if not "CC" in x]
+
+#use if storage is running out - deletes the cc folders
+def weed_out_del(DDSM):
+    for i in DDSM:
+        if "CC" in i:
+            shutil.rmtree(i, ignore_errors=true)
 
