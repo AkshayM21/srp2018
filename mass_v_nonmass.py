f0983549580c5a7511bb55b0273cb93cb1b77ca0
Andy Lee <21andyl@students.harker.org>
2018-03-11 14:14:41 -0700

    mass_v_nonmass
    -function

--- mass_v_nonmass.py
+++ mass_v_nonmass.py
@@ -9,45 +9,38 @@ roi_split = []
 non_mass = []
 
 
-def ROI_Split(DDSM, DDSM_ROI):
 
-
-	for i in range(len(DDSM)):
-		
-        ddsmDICOM = pydicom.dcmread(DDSM[i])
-        ddsmroiDICOM = pydicom.dcmread(DDSM_ROI[i])
-
-		ROI_Array = ddsmDICOM.pixel_array * ddsmroiDICOM.pixel_array
-		mass.append(ROI_Array)
-
-
-
-#CHANGE PIXEL TO BE PART OF THE ARRAY AND CREAT AN INVERTED ARRAY
-def ROI_Inverse(DDSM_ROI):
-
-
+for i in range(len(DDSM)):
 	
-	for i in range(len(DDSM_ROI)):
-        ddsmroiDICOM = pydicom.dcmread(DDSM_ROI[i])
+    ddsmDICOM = pydicom.dcmread(DDSM[i])
+    ddsmroiDICOM = pydicom.dcmread(DDSM_ROI[i])
 
-		for pixel in ddsmroiDICOM.pixel_array:
-			if pixel = 1:
-				pixel = 0
-			if pixel = 0:
-				pixel = 1
-			if x == ds.pixel_array.shape[1] - 1:
-                y = y + 1
-                x = 0
-                continue
-            x += 1
-        roi_split.append(ddsmroiDICOM.pixelarray)
+	ROI_Array = ddsmDICOM.pixel_array * ddsmroiDICOM.pixel_array
+	mass.append(ROI_Array)
 
 
 
-def DDSM_Split(DDSM):
-	for i in range(len(DDSM)):
-		
-        ddsmDICOM = pydicom.dcmread(DDSM[i])
 
-		Invert_Array = ddsmDICOM.pixel_array * roi_split[i]
-		non_mass.append(Invert_Array)
+for i in range(len(DDSM_ROI)):
+    ddsmroiDICOM = pydicom.dcmread(DDSM_ROI[i])
+
+	for pixel in ddsmroiDICOM.pixel_array:
+		if pixel = 1:
+			pixel = 0
+		if pixel = 0:
+			pixel = 1
+		if x == ds.pixel_array.shape[1] - 1:
+            y = y + 1
+            x = 0
+            continue
+        x += 1
+    roi_split.append(ddsmroiDICOM.pixelarray)
+
+
+
+for i in range(len(DDSM)):
+	
+    ddsmDICOM = pydicom.dcmread(DDSM[i])
+
+	Invert_Array = ddsmDICOM.pixel_array * roi_split[i]
+	non_mass.append(Invert_Array)
